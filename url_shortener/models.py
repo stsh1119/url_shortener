db333c292d127683527f4bc198aec884fd10e976
stsh1119 <stshlaptop@gmail.com>
2021-05-08 19:30:28 +0300

    + DB models

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -1,3 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+
+
+class Url(db.Model):
+    __tablename__ = 'urls'
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(200), unique=True, nullable=False)
+    short_url = db.Column(db.String(10), unique=True)
+    created_at = db.Column(db.DateTime())
+    days_to_expire = db.Column(db.Integer, default=90)
+    valid_until = db.Column(db.DateTime())
+
+    def __repr__(self):
+        return f"Url('{self.original_url}', '{self.short_url}', '{self.created_at}', '{self.days_to_expire}')"
