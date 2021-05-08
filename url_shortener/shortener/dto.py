b42d72bc4d2e4f689ea92758b3856dde82fbe1c4
stsh1119 <stshlaptop@gmail.com>
2021-05-08 22:19:49 +0300

    + DTO and validatiors for creation of new short link

---
+++ url_shortener/shortener/dto.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from pydantic import BaseModel, Field, validator
+
+
+class CreateNewLinkDto(BaseModel):
+    original_url: str = Field(min_length=10, max_length=200)
+    days_to_expire: Optional[int]
+
+    @validator('days_to_expire')
+    def should_be_between_1_and_365(cls, v):
+        if 1 <= v <= 365:
+            return v
+        raise ValueError('days_to_expire should be between 1 and 365')
+
+    @validator('original_url')
+    def should_start_with_http(cls, v):
+        if v.startswith(('http://', 'https://')):
+            return v
+        raise ValueError('links should start with http/https://')
