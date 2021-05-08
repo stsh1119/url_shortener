3f22018615ad4f86491839bf40839613b734ec2a
stsh1119 <stshlaptop@gmail.com>
2021-05-08 22:18:26 +0300

    added NOT NULL constrains for urls

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -7,10 +7,10 @@ class Url(db.Model):
     __tablename__ = 'urls'
     id = db.Column(db.Integer, primary_key=True)
     original_url = db.Column(db.String(200), unique=True, nullable=False)
-    short_url = db.Column(db.String(10), unique=True)
-    created_at = db.Column(db.DateTime())
-    days_to_expire = db.Column(db.Integer, default=90)
-    valid_until = db.Column(db.DateTime())
+    short_url = db.Column(db.String(10), unique=True, nullable=False)
+    created_at = db.Column(db.DateTime(), nullable=False)
+    days_to_expire = db.Column(db.Integer, nullable=False, default=90)
+    valid_until = db.Column(db.DateTime(), nullable=False)
 
     def __repr__(self):
         return f"Url('{self.original_url}', '{self.short_url}', '{self.created_at}', '{self.days_to_expire}')"
