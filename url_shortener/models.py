72697607177a4fd6c315c90cbaae0d0ae9b3b099
stsh1119 <stshlaptop@gmail.com>
2021-05-14 16:55:51 +0300

    Increased link lenght up to 2000 chars, removed original link uniqueness

--- url_shortener/models.py
+++ url_shortener/models.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 class Url(db.Model):
     __tablename__ = 'urls'
     id = db.Column(db.Integer, primary_key=True)
-    original_url = db.Column(db.String(200), unique=True, nullable=False)
+    original_url = db.Column(db.String(2000), nullable=False)
     short_url = db.Column(db.String(10), unique=True, nullable=False)
     created_at = db.Column(db.DateTime(), nullable=False)
     days_to_expire = db.Column(db.Integer, nullable=False, default=90)
